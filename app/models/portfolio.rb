efdfa63091c9e2acbaf1e6ba11b464b06652db17
Ryan Bertram <bertramdryan@gmail.com>
2021-07-12 16:41:54 -0400

    implimented concern for managing data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -12,6 +12,7 @@
 #  updated_at  :datetime         not null
 #
 class Portfolio < ApplicationRecord
+    include Placeholder
     validates_presence_of :title, :body, :main_image, :thumb_image
 
     def self.angular
@@ -24,7 +25,7 @@ class Portfolio < ApplicationRecord
 
 
     def set_defaults
-        self.main_image ||= "http://via.placeholder.com/600x400"
-        self.thumb_image ||= "http://via.placeholder.com/350x200"
+        self.main_image ||=  Placeholder.image_generator(height: '600', width: '400')
+        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
     end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -9,5 +9,12 @@
 #  updated_at       :datetime         not null
 #
 class Skill < ApplicationRecord
+    include Placeholder
     validates_presence_of :title, :percent_utilized
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.main_image ||= Placeholder.image_generator(height: 250, width: 250)
+    end
 end
